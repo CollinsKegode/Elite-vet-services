d1bd8026938cbb889706ba2fc2892c005e62e253
Collins Kegode <collinskegod@gmail.com>
2025-05-23 09:03:47 +0300

    add main entry of express application

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import session from 'express-session';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+import indexRoutes from './routes/index.js';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(session({
+  secret: 'kolee',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/', indexRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
